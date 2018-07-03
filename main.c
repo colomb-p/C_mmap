4ba6b7e18d577137b6557346a223b15b3b114c2c
colomb-p <pi.colombon@gmail.com>
2018-07-03 11:52:09 +0000

    removed mmap useless tests

--- main.c
+++ main.c
@@ -17,10 +17,8 @@ int			main(int argc, char *argv[])
   struct stat		st;
   int			file_size;
   int			fd;
-  int			fd2;
   char			*path = "./test";
   void			*mapped_data;
-  void			*mapped_data2;
   
   /* Getting "" file size */
   if (!access(path, R_OK))
@@ -32,15 +30,9 @@ int			main(int argc, char *argv[])
   if ((fd = open(path, O_RDONLY, 0)) == -1)
     my_perror("Open failed");
 
-  if ((fd2 = open(path, O_RDONLY, 0)) == -1)
-    my_perror("Open failed");
-
   /* Populate read_only memory with the file content */
   if ((mapped_data = mmap(NULL, file_size, PROT_READ | PROT_EXEC, MAP_SHARED | MAP_POPULATE, fd, 0)) == (void *)-1)
     my_perror("Mmap failed");
-
-  if ((mapped_data2 = mmap(NULL, file_size, PROT_READ | PROT_EXEC, MAP_SHARED | MAP_POPULATE, fd2, 0)) == (void *)-1)
-    my_perror("Mmap failed");
-  printf("%p, %p\n", mapped_data, mapped_data2);
+  printf("%p\n", mapped_data);
   return (0);
 }
